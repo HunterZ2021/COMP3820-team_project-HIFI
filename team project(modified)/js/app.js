ce7482313a38f139175fafd43ae00c6fedcae722
QuanTran-spec <ltranminh.ieu2019@student.ie.edu>
2022-10-04 16:49:27 +1000

    Add questionnaire JSONs & cleaning
    Sample Questionnaire and blank QuestionnaireResponse resources have been added.
    Some spelling corrections in edit_isbar.html and added IDs to input blocks for accessing them with js

---
+++ team project(modified)/js/app.js
@@ -0,0 +1,72 @@
+function index_fetchForms(params) {
+    
+}
+
+
+function index_createDetailed(params) {
+    
+}
+
+
+function generalForm_save(params) {
+
+    // Retrieve answers from inputs
+    let answers = { 
+        "Recipient": document.querySelector('#Recipient'),
+        "Identify": document.querySelector('#Identify'),
+        "Situation": document.querySelector('#Situation'),
+        "Background": document.querySelector('#Background'),
+        "Assessment": document.querySelector('#Assessment'),
+        "Recommendation": document.querySelector('#Recommendation'),
+        "Observation": document.querySelector('#Observation')
+    }
+
+    // Create QuestionnaireResponse JSON
+    let Response = {
+        "resourceType": "QuestionnaireResponse",
+        "text": {
+            "status": "generated",
+            "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"></div>"
+        },
+        "status": "in-progress",
+        "questionnaire": "https://r4.smarthealthit.org/Questionnaire/1406287",
+        "subject": {
+            "reference": "https://r4.smarthealthit.org/Patient/476d3544-6370-479f-9ac9-c43434aae12f",
+            "type": "Patient"
+        },
+    
+        "item": [
+            {
+                "linkId": "Recipient",
+                "answer": [{"valueString": answers.Recipient}]
+            },
+            {
+                "linkId": "Identify",
+                "answer": [{"valueString": answers.Identify}]
+            },
+            {
+                "linkId": "Situation",
+                "answer": [{"valueString": answers.Situation}]
+            },
+            {
+                "linkId": "Background",
+                "answer": [{"valueString": answers.Background}]
+            },
+            {
+                "linkId": "Assessment",
+                "answer": [{"valueString": answers.Assessment}]
+            },
+            {
+                "linkId": "Recommendation",
+                "answer": [{"valueString": answers.Recommendation}]
+            },
+            {
+                "linkId": "Observation",
+                "answer": [{"valueString": answers.Observation}]
+            }
+        ]
+    }
+    
+    return Response
+}
+
